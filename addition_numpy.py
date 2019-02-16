1457a18b6a9ae54f32212e6eb3a35b58372e0852
||Night_Flyer|| <dutta.raahul@gmail.com>
2019-02-16 23:21:51 +0530

    Add files via upload

---
+++ addition_numpy.py
@@ -0,0 +1,10 @@
+import numpy as np
+#importing the numpy
+
+a  = np.array([1, 2, 3])
+b = np.array([4, 5 , 6])
+#intilizing two arrays with random number, a and b
+
+print(a + b)
+#print the array
+# rseult is: [5 7 9]
